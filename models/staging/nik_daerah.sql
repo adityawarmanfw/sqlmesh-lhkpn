5dbb0ac7ccd6f88b0774fb6b7e4f01bffb18d042
edo-showwcase <adityawarmanfw@gmail.com>
2023-07-07 23:34:56 +0700

    fix daerah & nik_daerah

--- models/staging/nik_daerah.sql
+++ models/staging/nik_daerah.sql
@@ -1,16 +1,16 @@
 MODEL (
   name staging.nik_daerah,
   dialect duckdb,
-  kind VIEW,
+  kind FULL,
   tags [staging]
 );
 
 SELECT 
-    id_data,
-    nik,
-    nama_provinsi,
-    nama_kota_kabupaten,
-    nama_kecamatan
+    id_data::VARCHAR,
+    nik::VARCHAR,
+    nama_provinsi::VARCHAR,
+    nama_kota_kabupaten::VARCHAR,
+    nama_kecamatan::VARCHAR
 FROM source.tabel_situs tabel_situs
 LEFT JOIN staging.daerah daerah
   ON LEFT(tabel_situs.nik, 6) = daerah.id_kecamatan
