ccacd14f1053b52a46612182a82db1c5a6c5c9c0
edo-showwcase <adityawarmanfw@gmail.com>
2023-07-07 16:26:14 +0700

    rename column to id_induk_harta

--- models/sources/harta.sql
+++ models/sources/harta.sql
@@ -7,10 +7,10 @@ MODEL (
 
 SELECT 
     data_id::VARCHAR AS id_data,   
-    id::BIGINT AS id_harta,  
-    parent_id::BIGINT AS id_orang_tua_harta,
-    UPPER(kategori)::VARCHAR AS kategori_harta,   
-    UPPER(harta_deskripsi)::VARCHAR AS deskripsi_harta,   
+    id::BIGINT AS id_harta,
+    parent_id::BIGINT AS id_induk_harta,
+    UPPER(kategori)::VARCHAR AS kategori_harta,
+    UPPER(harta_deskripsi)::VARCHAR AS deskripsi_harta,
     UPPER(harta_nilai)::BIGINT AS nilai_harta
 FROM read_parquet('../data/harta.parquet')
 
